84fe27542a1ca53df905b339a57ad54f5de8958f
qcuong98 <tranquoccuong@outlook.com.vn>
2018-11-03 17:13:06 +0700

    Initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Graphics2D
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.Panel1.SuspendLayout();
+            this.splitContainer.Panel2.SuspendLayout();
+            this.splitContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer.Name = "splitContainer";
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.Controls.Add(this.button1);
+            // 
+            // splitContainer.Panel2
+            // 
+            this.splitContainer.Panel2.Controls.Add(this.pictureBox);
+            this.splitContainer.Size = new System.Drawing.Size(929, 391);
+            this.splitContainer.SplitterDistance = 222;
+            this.splitContainer.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(24, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(693, 381);
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            this.pictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseUp);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 403);
+            this.Controls.Add(this.splitContainer);
+            this.Name = "MainForm";
+            this.Text = "2D Graphics";
+            this.splitContainer.Panel1.ResumeLayout(false);
+            this.splitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graphics2D
+{
+    public partial class MainForm : Form
+    {
+        private const int MAX_LAYER = 100;
+        private Graphics myGraphics;
+        private GraphicsPath[] myGraphicsPath;
+        int nGraphicsPath;
+        int x0, y0, x1, y1;
+        bool MouseDowned;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            /* white background */
+            int wid = pictureBox.ClientSize.Width;
+            int hgt = pictureBox.ClientSize.Height;
+            Bitmap bm = new Bitmap(wid, hgt);
+            using (Graphics gr = Graphics.FromImage(bm))
+            {
+                gr.Clear(Color.White);
+            }
+            pictureBox.Image = bm;
+            pictureBox.Refresh();
+            
+            nGraphicsPath = 0;
+            myGraphicsPath = new GraphicsPath[MAX_LAYER];
+        }
+
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            x0 = e.X;
+            y0 = e.Y;
+            MouseDowned = true;
+        }
+
+        private void pictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (MouseDowned)
+            {
+                x1 = e.X;
+                y1 = e.Y;
+                AddLine(new Point(x0, y0), new Point(x1, y1));
+                --nGraphicsPath;
+            }
+        }
+
+        private void pictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            x1 = e.X;
+            y1 = e.Y;
+            AddLine(new Point(x0, y0), new Point(x1, y1));
+            MouseDowned = false;
+        }
+
+        private void AddLine(Point a, Point b) {
+            Pen myPen = new Pen(Brushes.Red);
+            GraphicsPath tmpGraphicsPath = new GraphicsPath();
+            tmpGraphicsPath.AddLine(a, b);            
+
+            myGraphicsPath[nGraphicsPath] = tmpGraphicsPath;
+            ++nGraphicsPath;
+
+            tmpGraphicsPath = new GraphicsPath();
+
+            for (int i = 0; i < nGraphicsPath; ++i)
+            {
+                tmpGraphicsPath.AddPath(myGraphicsPath[i], false);
+            }
+
+            int wid = pictureBox.ClientSize.Width;
+            int hgt = pictureBox.ClientSize.Height;
+            Bitmap bm = new Bitmap(wid, hgt);
+            using (Graphics gr = Graphics.FromImage(bm))
+            {
+                gr.Clear(Color.White);
+                gr.DrawPath(myPen, tmpGraphicsPath);
+            }
+            pictureBox.Image = bm;
+            pictureBox.Refresh();
+        }
+
+
+    }
+}
